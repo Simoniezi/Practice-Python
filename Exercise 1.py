a39595548f1fa1f9b1e5cdad395c1b997fdbee3e
Simoniezi <73497522+Simoniezi@users.noreply.github.com>
2020-10-31 01:36:37 +0100

    Update Exercise 1.py

--- Exercise 1.py
+++ Exercise 1.py
@@ -5,7 +5,7 @@ name = input('What is your name: ')
 age = input('How old are you?: ')
 age = int(age)
 year = str((2020 - age) + 100)
-print(name + 'you will be 100 years in the year ' + year)
+print(name + ' you will be 100 years in the year ' + year)
 
 # Made by Simoniezi
 # Discord: Simoniezi#7138

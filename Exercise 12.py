dceacf684f69c65ddfac02fdf7f679fc187a527d
Simoniezi <73497522+Simoniezi@users.noreply.github.com>
2020-11-01 15:18:48 +0100

    Add files via upload

---
+++ Exercise 12.py
@@ -0,0 +1,15 @@
+# Exercise 12
+# List Ends
+
+a = [5, 10, 15, 20, 25]
+b = []
+
+def listEnds(a):
+    b.append(a[0])
+    b.append(a[-1])
+    print(b)
+
+listEnds(a)
+
+# Made by Simoniezi
+# Discord: Simoniezi#7138
